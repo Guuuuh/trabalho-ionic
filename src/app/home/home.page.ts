b792e706c0a29172f39ee8ce51b0ef42df2cd808
Guuuuh <gustavo.htl@hotmail.com>
2023-03-24 16:59:26 -0300

    Inicio do trabalho
    foi criado o header e iniciado a parte dos slides das categorias

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public categorias = [
+    {id: 1, nome: "Pizzas"},
+    {id: 2, nome: "Burgers"},
+    {id: 3, nome: "Salgados"},
+    {id: 4, nome: "Cachorrão"},
+    {id: 5, nome: "Sucos"}
+  ]
+
   constructor() {}
 
 }
